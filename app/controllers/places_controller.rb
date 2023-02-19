a36f0c52f37bf47c3bfc66408f38934690215016
mitchprosperi-kellogg <mitch.prosperi@kellogg.northwestern.edu>
2023-02-19 15:17:28 +0000

    Added show

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,12 +6,16 @@ class PlacesController < ApplicationController
       def new
         @place = Places.new
       end
+
+      def show
+        @place = Place.find_by({"id" => params["id"]})
+        @post = Post.where({ "post_id" => @post["id"] })
     
       def create
         @place = Place.new
-        @post["name"] = params["post"]["name"]
+        @post["name"] = params["place"]["name"]
         @post.save
-        redirect_to "/posts"
+        redirect_to "/places"
       end
 
 end
